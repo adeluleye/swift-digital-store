f675cac294b950a85d29ad1e023e4cd245fa0164
adeluleye <adelu4real@gmail.com>
2018-10-09 23:58:38 +0100

    Initial Commit

---
+++ digital-store/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  digital-store
+//
+//  Created by ADELU ABIDEEN ADELEYE on 10/9/18.
+//  Copyright © 2018 Spantom Technologies Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

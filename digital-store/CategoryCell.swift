dd6619482f4e7389de11226167809e8e73f34532
adeluleye <adelu4real@gmail.com>
2018-10-10 00:27:19 +0100

    move CategoryCell to a new file

---
+++ digital-store/CategoryCell.swift
@@ -0,0 +1,26 @@
+//
+//  CategoryCell.swift
+//  digital-store
+//
+//  Created by ADELU ABIDEEN ADELEYE on 10/10/18.
+//  Copyright © 2018 Spantom Technologies Ltd. All rights reserved.
+//
+
+import UIKit
+
+class CategoryCell: UICollectionViewCell {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupViews()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupViews() {
+        backgroundColor = .red
+    }
+    
+}
--- digital-store/FeaturedAppsController.swift
+++ digital-store/FeaturedAppsController.swift
@@ -40,20 +40,3 @@ class FeaturedAppsController: UICollectionViewController, UICollectionViewDelega
     }
 
 }
-
-class CategoryCell: UICollectionViewCell {
-    
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        setupViews()
-    }
-    
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    func setupViews() {
-        backgroundColor = .red
-    }
-    
-}

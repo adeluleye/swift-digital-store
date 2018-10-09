1ed01614850bdd059b49f2ad6ef676bd6afed34b
adeluleye <adelu4real@gmail.com>
2018-10-10 00:20:51 +0100

    implement cv functions, create a cell and cast d cv to the cell, register the cv in viewDidLoad

--- digital-store/FeaturedAppsController.swift
+++ digital-store/FeaturedAppsController.swift
@@ -9,14 +9,46 @@
 import UIKit
 
 class FeaturedAppsController: UICollectionViewController {
+    
+    let cellId = "cellId"
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        collectionView.backgroundColor = .red
+        collectionView.backgroundColor = .white
+        
+        collectionView.register(CategoryCell.self, forCellWithReuseIdentifier: cellId)
     }
-
+    
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        return 3
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! CategoryCell
+        
+        return cell
+    }
 
 }
 
+class CategoryCell: UICollectionViewCell {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupViews()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupViews() {
+        backgroundColor = .red
+    }
+    
+}

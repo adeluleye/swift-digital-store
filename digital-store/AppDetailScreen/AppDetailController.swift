42ae06a95ac41a0a5c6debf4378a483ebc531479
adeluleye <adelu4real@gmail.com>
2018-10-12 00:48:59 +0100

    add divider line view as subview in the AppDetailDescriptionCell

--- digital-store/AppDetailScreen/AppDetailController.swift
+++ digital-store/AppDetailScreen/AppDetailController.swift
@@ -140,12 +140,21 @@ class AppDetailDescriptionCell: BaseCell {
         return textView
     }()
     
+    let dividerLineView: UIView = {
+        let view = UIView(frame: .zero)
+        view.backgroundColor = UIColor(white: 0.4, alpha: 0.4)
+        return view
+    }()
+    
     override func setupViews() {
         super.setupViews()
         
         addSubview(descriptionTextView)
+        addSubview(dividerLineView)
         
         descriptionTextView.setAnchor(top: topAnchor, topPad: 0, bottom: nil, bottomPad: 0, left: leftAnchor, leftPad: 14, right: rightAnchor, rightPad: 0, height: 0, width: 0)
+        
+        dividerLineView.setAnchor(top: nil, topPad: 0, bottom: bottomAnchor, bottomPad: 0, left: leftAnchor, leftPad: 14, right: rightAnchor, rightPad: 0, height: 0.5, width: 0)
     }
     
 }

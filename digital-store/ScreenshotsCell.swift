3ba1f7aa980eabc715c6f17595960cc20ff5b122
adeluleye <adelu4real@gmail.com>
2018-10-11 20:43:49 +0100

    implement the delegate and datasource of the cv in Screen shot cell, register a new class for screen shot image cell in the cv

--- digital-store/ScreenshotsCell.swift
+++ digital-store/ScreenshotsCell.swift
@@ -8,7 +8,9 @@
 
 import UIKit
 
-class ScreenshotsCell: BaseCell {
+class ScreenshotsCell: BaseCell, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    private let cellId = "cellId"
     
     let collectionView: UICollectionView = {
         let layout = UICollectionViewFlowLayout()
@@ -22,9 +24,33 @@ class ScreenshotsCell: BaseCell {
     override func setupViews() {
         super.setupViews()
         
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        
+        collectionView.register(ScreenshotImageCell.self, forCellWithReuseIdentifier: cellId)
+        
         addSubview(collectionView)
         
         collectionView.setAnchor(top: topAnchor, topPad: 0, bottom: bottomAnchor, bottomPad: 0, left: leftAnchor, leftPad: 0, right: rightAnchor, rightPad: 0, height: 0, width: 0)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! ScreenshotImageCell
+        return cell
+    }
+}
+
+class ScreenshotImageCell: BaseCell {
+    
+    override func setupViews() {
+        super.setupViews()
+        
+        backgroundColor = .yellow
     }
     
 }

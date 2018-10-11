02f4568669f74c629427e76f0592c73a3f2fffb4
adeluleye <adelu4real@gmail.com>
2018-10-11 23:39:09 +0100

    move the Header in featured app controller to a new file

--- digital-store/FeaturedAppsController.swift
+++ digital-store/FeaturedAppsController.swift
@@ -106,51 +106,6 @@ class FeaturedAppsController: UICollectionViewController, UICollectionViewDelega
 
 }
 
-class Header: CategoryCell {
-    
-    let bannerCellId = "bannerCellId"
-    
-    override func setupViews() {
-        // super.setupViews()
-        
-        appsCollectionView.delegate = self
-        appsCollectionView.dataSource = self
-        
-        addSubview(appsCollectionView)
-        appsCollectionView.setAnchor(top: topAnchor, topPad: 0, bottom: bottomAnchor, bottomPad: 0, left: leftAnchor, leftPad: 0, right: rightAnchor, rightPad: 0, height: 0, width: 0)
-        appsCollectionView.register(BannerCell.self, forCellWithReuseIdentifier: bannerCellId)
-    }
-    
-    override func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-        return UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
-    }
-    
-    override func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        
-        return CGSize(width: (frame.width / 2) + 50, height: frame.height)
-        
-    }
-    
-    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: bannerCellId, for: indexPath) as! BannerCell
-        
-        cell.app = appCategory?.apps?[indexPath.item]
-        return cell
-    }
-    
-    private class BannerCell: AppCell {
-        override func setupViews() {
-            addSubview(imageView)
-            imageView.layer.borderColor = UIColor(white: 0.5, alpha: 0.5).cgColor
-            imageView.layer.borderWidth = 0.5
-            imageView.layer.cornerRadius = 0
-            imageView.setAnchor(top: topAnchor, topPad: 0, bottom: bottomAnchor, bottomPad: 0, left: leftAnchor, leftPad: 0, right: rightAnchor, rightPad: 0, height: 0, width: 0)
-        }
-    }
-    
-}
-
 class LargeCategoryCell: CategoryCell {
     
     private let largeAppCellId = "largeAppCellId"
